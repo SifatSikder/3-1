788b10b0a50c702b686842e7980944bc820f5bed
Sifat Sikder <67006643+SifatSikder@users.noreply.github.com>
2023-01-03 12:40:23 +0600

    Update Document.java

--- Design Pattern/printing_management_assignment/refactored_printing_management_system/Document.java
+++ Design Pattern/printing_management_assignment/refactored_printing_management_system/Document.java
@@ -1,3 +1,9 @@
 package refactored_printing_management_system;
 
-class Document {}
+class Document {
+    
+  //solved another black sheep
+  void renderPreview() {
+      // show a preview of the updated document
+    }
+}
