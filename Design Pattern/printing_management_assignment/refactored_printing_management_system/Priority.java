a0921cbeacd1634a0400c4f21690721887acd210
Sifat Sikder <67006643+SifatSikder@users.noreply.github.com>
2023-01-03 12:25:09 +0600

    Update Priority.java

--- Design Pattern/printing_management_assignment/refactored_printing_management_system/Priority.java
+++ Design Pattern/printing_management_assignment/refactored_printing_management_system/Priority.java
@@ -1,10 +1,25 @@
 package refactored_printing_management_system;
 
-class Priority {
+class PrioritySetting {
     private PriorityLevel priorityLevel;
     
     // getter and setter for priorityLevel
+    public PriorityLevel getPriorityLevel() {
+        return priorityLevel;
+    }
+    
+    public void setPriorityLevel(PriorityLevel priorityLevel) {
+        this.priorityLevel = priorityLevel;
   }
+  
+    
+  //solved blacksheep
+  private void changePriority()
+  {
+    //change the priority of a job respectively. 
+    
+  }
+}
 enum PriorityLevel
 {
   HIGH,
